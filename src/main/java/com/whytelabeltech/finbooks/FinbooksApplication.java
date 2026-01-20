4195016a0a662fe05e69b693aa8e6a1a07b3a9d4
Dev-JoyA <joy.gold13@gmail.com>
2026-01-20 21:40:39 +0000

    Initial project setup

---
+++ src/main/java/com/whytelabeltech/finbooks/FinbooksApplication.java
@@ -0,0 +1,15 @@
+package com.whytelabeltech.finbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinbooksApplication {
+
+	public static void main(String[] args) {
+        System.out.println("Hello world");
+		
+        SpringApplication.run(FinbooksApplication.class, args);
+	}
+
+}
