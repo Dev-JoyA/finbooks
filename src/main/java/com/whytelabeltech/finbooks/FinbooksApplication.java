9b28e31b7a14745dbf93448a46cf71d0d79c97c3
Dev-JoyA <joy.gold13@gmail.com>
2026-01-21 04:39:33 +0000

    database setup done

--- src/main/java/com/whytelabeltech/finbooks/FinbooksApplication.java
+++ src/main/java/com/whytelabeltech/finbooks/FinbooksApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinbooksApplication {
 
 	public static void main(String[] args) {
-        System.out.println("Hello world");
-		
+
         SpringApplication.run(FinbooksApplication.class, args);
 	}
 
